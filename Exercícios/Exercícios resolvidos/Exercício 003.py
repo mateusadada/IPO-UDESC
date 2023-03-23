c6639d3115576b3417d1d7dab89e41da794d22f0
Mateus Adada <mateusadada@gmail.com>
2023-03-22 23:41:41 -0300

    Create Exercício 003.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 003.py
@@ -0,0 +1,7 @@
+# Receber o ano de nascimento e imprimir a idade em 2023.
+
+ano_de_nascimento = int(input('Digite o ano de nascimento: '))
+
+idade = 2023 - ano_de_nascimento
+
+print(f'\nA idade em 2023 de quem nasceu em {ano_de_nascimento} é {idade}')
