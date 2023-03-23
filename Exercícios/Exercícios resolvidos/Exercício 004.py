84c31ae0608126f04ee5651cf7171c20096464a9
Mateus Adada <mateusadada@gmail.com>
2023-03-22 23:46:53 -0300

    Create Exercício 004.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 004.py
@@ -0,0 +1,14 @@
+# Receber 2 número e imprimir a soma, a subtração, multiplicação e divisão entre eles.
+
+numero1 = float(input('Digite o primeiro número: '))
+numero2 = float(input('Digite o segundo número: '))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print(f'\nSoma: {soma:.2f}'
+      f'\nSubtração: {subtracao:.2f}'
+      f'\nMultiplicação: {multiplicacao:.2f}'
+      f'\nDivisão: {divisao:.2f}')
