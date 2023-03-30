ae4912d6dfd8148ede4eac96c57b07b0e7ce8b12
Mateus Adada <mateusadada@gmail.com>
2023-03-30 18:40:10 -0300

    Create Exercício 009.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 009.py
@@ -0,0 +1,19 @@
+# Exemplo 5 do PDF Slides da aula - Parte 5
+
+categoria = int(input('Digite a categoria do produto: '))
+
+if categoria == 1:
+    preco = 10
+elif categoria == 2:
+    preco = 18
+elif categoria == 3:
+    preco = 23
+elif categoria == 4:
+    preco = 26
+elif categoria == 5:
+    preco = 31
+else:
+    print('\nCategoria inválida!')
+    preco = 0
+
+print(f'\nO preço do produto é de R$ {preco:.2f} reais')
