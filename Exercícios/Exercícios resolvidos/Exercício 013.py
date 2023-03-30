b4ef66b53a4b7c55344fd7810ba595b3c414c2cb
Mateus Adada <mateusadada@gmail.com>
2023-03-30 20:26:06 -0300

    Update Exercício 013.py
    Incluído o código e deixado mais formal a observação

--- Exercícios/Exercícios resolvidos/Exercício 013.py
+++ Exercícios/Exercícios resolvidos/Exercício 013.py
@@ -1,2 +1,13 @@
 # Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-Matutino, V-Vespertino
-# ou N-Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!", "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+# ou N-Noturno. Imprima a mensagem "Bom dia!", "Boa tarde!", "Boa noite!" ou "Valor inválido!", conforme o caso.
+
+turno = input('Em qual turno você trabalha (M-Matutino, V-Vespertino ou N-Noturno)? ')
+
+if turno == 'M':
+    print('\nBom dia!')
+elif turno == 'V':
+    print('\nBoa tarde!')
+elif turno == 'N':
+    print('\nBoa noite!')
+else:
+    print('\nValor inválido!')
