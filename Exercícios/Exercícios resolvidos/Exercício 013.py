3842d96819dfae1b017db0396ba70d2db8d775bd
Mateus Adada <mateusadada@gmail.com>
2023-03-30 19:51:48 -0300

    Update Exercício 013.py
    Letra maiúscula

--- Exercícios/Exercícios resolvidos/Exercício 013.py
+++ Exercícios/Exercícios resolvidos/Exercício 013.py
@@ -1,2 +1,2 @@
-# Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino, V-Vespertino
+# Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-Matutino, V-Vespertino
 # ou N-Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!", "Boa Noite!" ou "Valor Inválido!", conforme o caso.
