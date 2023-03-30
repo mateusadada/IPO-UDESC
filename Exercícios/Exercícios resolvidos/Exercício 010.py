b59ec27089b73729c215ecba6ac2f8a9d8406886
Mateus Adada <mateusadada@gmail.com>
2023-03-30 19:42:08 -0300

    Create Exercícios 010, 011, 012 e 013.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 010.py
@@ -0,0 +1,1 @@
+# Faça um programa que leia três números inteiros, em seguida mostre o maior e o menor deles.
---
+++ Exercícios/Exercícios resolvidos/Exercício 013.py
@@ -0,0 +1,2 @@
+# Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino, V-Vespertino
+# ou N-Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!", "Boa Noite!" ou "Valor Inválido!", conforme o caso.
