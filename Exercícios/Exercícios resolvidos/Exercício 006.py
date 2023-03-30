057bb7e4678f81f8dbccc362bd37ad1b688b841e
Mateus Adada <mateusadada@gmail.com>
2023-03-30 18:22:31 -0300

    Create Exercício 006.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 006.py
@@ -0,0 +1,8 @@
+# Exemplo 2 do PDF Slides da aula - Parte 5
+
+idade = int(input('Digite a idade do seu carro: '))
+
+if idade <= 3:
+    print('\nSeu carro é seminovo')
+if idade > 3:
+    print('\nSeu carro é usado')
