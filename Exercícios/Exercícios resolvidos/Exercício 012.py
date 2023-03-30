cd3c9e3b76d6495910b0c429426642e8b54f6471
Mateus Adada <mateusadada@gmail.com>
2023-03-30 20:20:14 -0300

    Update Exercício 012.py
    Incluído o código e retificado quanto à norma culta da língua portuguesa

--- Exercícios/Exercícios resolvidos/Exercício 012.py
+++ Exercícios/Exercícios resolvidos/Exercício 012.py
@@ -1,1 +1,24 @@
-# Faça um Programa que leia três números e mostre-os em ordem decrescente. Três números em ordem decrescente
+# Faça um programa que leia três números e mostre-os em ordem decrescente.
+
+a = int(input('Digite o 1º número: '))
+b = int(input('Digite o 2º número: '))
+c = int(input('Digite o 3º número: '))
+
+print(f'\nOrdem digitada: {a}, {b} e {c}')
+
+if a < b:
+    aux = a
+    a = b
+    b = aux
+
+if a < c:
+    aux = a
+    a = c
+    c = aux
+
+if b < c:
+    aux = b
+    b = c
+    c = aux
+
+print(f'\nOrdem decrescente: {a}, {b} e {c}')
