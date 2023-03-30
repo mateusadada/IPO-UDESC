1287cd331860ac225d267819be993492bf205940
Mateus Adada <mateusadada@gmail.com>
2023-03-30 20:12:58 -0300

    Update Exercício 011.py
    Incluído o código

--- Exercícios/Exercícios resolvidos/Exercício 011.py
+++ Exercícios/Exercícios resolvidos/Exercício 011.py
@@ -1,2 +1,16 @@
 # Faça um programa que pede dois inteiro e armazene em duas variáveis. Em seguida troque o valor das variáveis
 # e exiba na tela - Trocar (inverter) valores de duas variáveis.
+
+a = int(input('Digite o 1º número: '))
+b = int(input('Digite o 2º número: '))
+
+print(f'\n1º valor = {a}'
+      f'\n2º valor = {b}'
+      '\n\n*** INVERTENDO ***')
+
+aux = a
+a = b
+b = aux
+
+print(f'\n1º valor = {a}'
+      f'\n2º valor = {b}')
