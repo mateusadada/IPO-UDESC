c5e9a9045fda33b311be2907cfa7e3767321267e
Mateus Adada <mateusadada@gmail.com>
2023-03-30 18:19:35 -0300

    Create Exercício 005.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 005.py
@@ -0,0 +1,10 @@
+# Exemplo 1 do PDF Slides da aula - Parte 5
+
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+
+if a > b:
+    print('\nO primeiro número é maior!')
+
+if b > a:
+    print('\nO segundo número é maior!')
