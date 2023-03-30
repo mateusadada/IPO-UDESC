18be69153226241ad6d93557acae26bd6cef797d
Mateus Adada <mateusadada@gmail.com>
2023-03-30 18:27:58 -0300

    Create Exercício 007.py

---
+++ Exercícios/Exercícios resolvidos/Exercício 007.py
@@ -0,0 +1,9 @@
+# Exemplo 3 do PDF Slides da aula - Parte 5
+# Mesmo exercício que o anterior (exemplo 2), mas utilizando o else
+
+idade = int(input('Digite a idade do seu carro: '))
+
+if idade <= 3:
+    print('\nSeu carro é seminovo')
+else:
+    print('\nSeu carro é usado')
