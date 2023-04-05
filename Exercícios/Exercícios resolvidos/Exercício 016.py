4fbc828c126c2d69d48fbbc902db4ac4120d9bc5
Mateus Adada <mateusadada@gmail.com>
2023-04-05 19:59:04 -0300

    Update Exercício 016.py
    Incluído o time

--- Exercícios/Exercícios resolvidos/Exercício 016.py
+++ Exercícios/Exercícios resolvidos/Exercício 016.py
@@ -1,9 +1,13 @@
 # Faça um programa para escrever a contagem regressiva do lançamento de um foguete.
 # O programa deve imprimir 10, 9, 8, ..., 1, 0 e FOGO! na tela
 
+import time
+
 x = 10
 
 while x >= 0:
     print(x)
+    time.sleep(1)
     x -= 1
+
 print('FOGO!!!')
