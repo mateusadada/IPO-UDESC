7a254123eff1a7b3d4217534748f8ec37cfee6f5
Mateus Adada <mateusadada@gmail.com>
2023-04-05 20:11:44 -0300

    Update Exercícios 017 e 018.py
    Incluído o código

--- Exercícios/Exercícios resolvidos/Exercício 017.py
+++ Exercícios/Exercícios resolvidos/Exercício 017.py
@@ -1,1 +1,8 @@
 # Faça um programa para imprimir os números ímpares de 1 até o número digitado pelo usuário
+
+fim = int(input('Digite um número positivo: '))
+x = 1
+
+while x <= fim:
+    print(x)
+    x += 2
--- Exercícios/Exercícios resolvidos/Exercício 018.py
+++ Exercícios/Exercícios resolvidos/Exercício 018.py
@@ -1,1 +1,10 @@
 # Faça um programa para escrever os 10 primeiros múltiplos de 3
+
+import time
+
+x = 3
+
+while x <= 30:
+    print(x)
+    x += 3
+    time.sleep(0.3)
