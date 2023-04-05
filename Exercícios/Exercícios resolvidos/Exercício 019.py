5a3835d2ceb525e25cc9307e566dd8508674d4cb
Mateus Adada <mateusadada@gmail.com>
2023-04-05 20:29:03 -0300

    Update Exercício 019.py
    Incluído o código

--- Exercícios/Exercícios resolvidos/Exercício 019.py
+++ Exercícios/Exercícios resolvidos/Exercício 019.py
@@ -1,1 +1,10 @@
 # Imprimir uma tabuada (de 1 a 10) de adiação de um número digitado pelo usuário
+
+numero = int(input('Digite um número: '))
+x = 1
+
+print('\n*** TABUADA ***')
+
+while x <= 10:
+    print(f'{numero} x {x} = {numero * x}')
+    x += 1
