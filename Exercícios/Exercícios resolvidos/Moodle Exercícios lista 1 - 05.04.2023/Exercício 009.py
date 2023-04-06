28a37c80969e72492bb391435f589fe499be249a
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-06 10:36:11 -0300

    Create Exercício 009.py
    Added the note

---
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 009.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia dois números inteiros e depois mostre:
+#  - o primeiro valor vezes o segundo
+#  - o segundo valor menos o primeiro
+#  - o inverso do primeiro valor vezes o segundo multiplicado por 5
+#  - o quadrado do primeiro dividido pelo segundo elevado a -2
