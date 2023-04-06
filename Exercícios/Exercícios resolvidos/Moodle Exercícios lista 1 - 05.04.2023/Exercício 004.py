69d46ab4faaf81fbfd46a80b0fc83d5ef0ad9346
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-06 10:32:10 -0300

    Create Exercício 004.py
    Adicionado a observação

---
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 004.py
@@ -0,0 +1,3 @@
+# Faça um programa para receber as strings de um madlib. Utilize o seguinte texto como base:
+# hoje, eu fui no (lugar) com meu (substantivo). Fizemos muitas compras, já que em casa estava faltando (substantivo).
+# Depois disso, a gente foi na praia, onde vimos muitas pessoas (adjetivo) (verbo no gerúndio)
