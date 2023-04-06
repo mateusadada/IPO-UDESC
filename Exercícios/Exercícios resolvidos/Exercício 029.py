1ad86171d9d04d82b3c5ef71daf77a11b782f27d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-06 09:46:05 -0300

    Create Exercício 029.py
    Adicionado a observação

---
+++ Exercícios/Exercícios resolvidos/Exercício 029.py
@@ -0,0 +1,2 @@
+# Faça um programa que exiba uma lista de opções (menu): adição, subtração, divisão, multiplicação e sair.
+# Imprima a tabuada da operação escolhida. Repita até que a opção sair seja escolhida
