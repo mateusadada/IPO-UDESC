061a9dbf43449811676643cc85e71a6f4a9b4391
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-06 10:35:33 -0300

    Create Exercício 008.py
    Added the note

---
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 008.py
@@ -0,0 +1,7 @@
+# Desenvolva um programa para calcular o custo e quantas latas de tintas serão utilizados para pintar tanques 
+# cilíndricos de combustível sendo que:
+#  - a lata de tinta custa R$120,00
+#  - cada lata contém 5 litros
+#  - cada litro pinta 4m²
+#  - a altura e o raio do tanque serão informados pelo usuário
+#  - mostre na tela a área do tanque, a quantidade de latas e o custo total
