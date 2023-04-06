a3cf952fef3af89cb0147bf318a3314a158e2c0b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-06 09:43:58 -0300

    Create Exercício 027.py
    Adicionado a observação

---
+++ Exercícios/Exercícios resolvidos/Exercício 027.py
@@ -0,0 +1,2 @@
+# Faça um programa que leia números inteiros do teclado, até que o usuário digite 0 (zero). No final da execução, exiba a quantidade de
+# números digitados, assim como a soma e a média aritmética
