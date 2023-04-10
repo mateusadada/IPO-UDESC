b813175f0961cd3b2e897e1abc34db63ccccdee5
Mateus Adada <mateusadada@gmail.com>
2023-04-10 18:07:28 -0300

    Update Exercício 023.py
    Replaced variavel and added divisão por 5 para fazer a média

--- Exercícios/Exercícios resolvidos/Exercício 023.py
+++ Exercícios/Exercícios resolvidos/Exercício 023.py
@@ -1,11 +1,11 @@
 # Faça um programa para calcular e imprimir a média de 5 números digitados pelo usuário
 
 contador = 1
-media_total = 0
+soma_total = 0
 
 while contador <= 5:
     numero = int(input(f'Digite um número ({contador}/5): '))
-    media_total += numero
+    soma_total += numero
     contador += 1
 
-print(f'\nA média dos cinco números é igual a {media_total:.2f}')
+print(f'\nA média dos cinco números é igual a {soma_total/5:.2f}')
