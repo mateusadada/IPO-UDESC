2aacd56130fe83bf8c9b259afe2fb02f3a423920
Mateus Adada <mateusadada@gmail.com>
2023-04-10 17:59:13 -0300

    Update Exercício 023.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 023.py
+++ Exercícios/Exercícios resolvidos/Exercício 023.py
@@ -1,1 +1,11 @@
 # Faça um programa para calcular e imprimir a média de 5 números digitados pelo usuário
+
+contador = 1
+media_total = 0
+
+while contador <= 5:
+    numero = int(input(f'Digite um número ({contador}/5): '))
+    media_total += numero
+    contador += 1
+
+print(f'\nA média dos cinco números é igual a {media_total:.2f}')
