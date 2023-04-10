dbd6278a5a0648e1fd71aeed75c033315db3aa8f
Mateus Adada <mateusadada@gmail.com>
2023-04-09 22:50:11 -0300

    Update Exercícios 001, 024, 025, 027 and 030
    Added spaces

--- Exercícios/Exercícios resolvidos/Exercício 024.py
+++ Exercícios/Exercícios resolvidos/Exercício 024.py
@@ -1,2 +1,2 @@
-# Faça um programa que pergunte o depósito inicial e a taxa de juros de uma poupança. Exiba os valores mês a mês para os 12 primeiros meses.
-# Escreva o total ganho com juros no período
+# Faça um programa que pergunte o depósito inicial e a taxa de juros de uma poupança.
+# Exiba os valores mês a mês para os 12 primeiros meses. Escreva o total ganho com juros no período
