bb9a87576457c471fef094de1f0e1bd339a6c242
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-11 15:53:11 -0300

    Update Exercício 026.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 026.py
+++ Exercícios/Exercícios resolvidos/Exercício 026.py
@@ -1,1 +1,11 @@
 # Faça um programa para somar vários números até que o usuário digite 0 (zero) para parar a leitura
+
+soma = 0
+
+while True:
+    numero = int(input('Digite um número (0 p/ sair): '))
+    if numero == 0:
+        break
+    soma += numero
+
+print(f'\nA soma de todos os número é {soma}')
