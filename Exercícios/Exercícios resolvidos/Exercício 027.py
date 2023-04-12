fc119fecb1b96d86c133fd193f2dcf9a04648ff1
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-12 14:29:08 -0300

    Update Exercício 027.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 027.py
+++ Exercícios/Exercícios resolvidos/Exercício 027.py
@@ -1,2 +1,17 @@
 # Faça um programa que leia números inteiros do teclado, até que o usuário digite 0 (zero).
 # No final da execução, exiba a quantidade de números digitados, assim como a soma e a média aritmética
+
+soma = 0
+contador = 0
+
+while True:
+    numero = int(input('Digite um número inteiro (0 p/ sair): '))
+    if numero == 0:
+        break
+    soma += numero
+    contador += 1
+
+print('\n*** DADOS ***'
+      f'\nQuantidade de números digitados: {contador}'
+      f'\nSoma: {soma}'
+      f'\nMédia aritmética: {soma / contador:.2f}')
