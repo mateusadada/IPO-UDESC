5b1373b556086f771aa197b9f1417279fc034863
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-12 14:35:06 -0300

    Update Exercício 028.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 028.py
+++ Exercícios/Exercícios resolvidos/Exercício 028.py
@@ -1,1 +1,11 @@
 # Imprimir as tabuadas de multiplicação de 1 a 10
+
+i = 1
+
+while i <= 10:
+    print(f'\n*** TABUADA DO {i} ***')
+    j = 1
+    while j <= 10:
+        print(f'{i} x {j} = {i * j}')
+        j += 1
+    i += 1
