4dba78abeb701288710b91dda3c610923520381f
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-13 16:38:17 -0300

    Update Exercício 031.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 031.py
+++ Exercícios/Exercícios resolvidos/Exercício 031.py
@@ -1,1 +1,25 @@
 # Modifique o programa anterior de forma a ler um número n. Imprima os n primeiros números primos
+
+quantidade = int(input('Deseja ver quantos números primos? '))
+numero = 3
+contador = 0
+
+if quantidade < 1:
+    print('\nVocê optou por ver nenhum número primo :(')
+else:
+    print(f'\n*** LISTANDO OS {quantidade} PRIMEIROS NÚMEROS PRIMOS ***\n')
+    while contador < quantidade:
+        impar_divisor = 3
+        while True:
+            if numero == impar_divisor:
+                contador += 1
+                print(f'{contador}º primo: {numero}')
+                numero += 2
+                break
+
+            elif numero % impar_divisor == 0:
+                numero += 2
+                break
+
+            else:
+                impar_divisor += 2
