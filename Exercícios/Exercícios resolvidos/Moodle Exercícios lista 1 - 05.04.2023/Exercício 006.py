2eda5c0949f36ff2374c6681b2be082f56edf860
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-14 16:14:57 -0300

    Update Exercício 006.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 006.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 006.py
@@ -1,1 +1,7 @@
 # Receba uma base e um expoente de uma potência e calcule o resultado, exibindo-o na tela
+
+print('*** CÁLCULO DE POTÊNCIA ***')
+base = int(input('Digite a base: '))
+expoente = int(input('Digite o expoente: '))
+
+print(f'\nResultado da potência: {base ** expoente:.0f}')
