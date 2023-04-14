8a2f49e63898fa0d2896a9deabbf23be7a7de1d9
Mateus Adada <mateusadada@gmail.com>
2023-04-13 21:37:09 -0300

    Update Exercício 001.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 001.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 001.py
@@ -1,1 +1,13 @@
 # Receba três notas de usuários e calcule a média simples, exibindo o resultado na tela
+
+i = 1
+soma = 0
+
+while i <= 3:
+    numero = float(input(f'Digite a {i}ª nota: '))
+    soma += numero
+    i += 1
+
+media = soma / 3
+
+print(f'\nA média das três notas é {media:.2f}')
