f3b1a4245b3a87bf3e97ec1b7e28c3c29e67fcab
Mateus Adada <mateusadada@gmail.com>
2023-04-13 22:05:46 -0300

    Update Exercício 003.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 003.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 003.py
@@ -1,1 +1,9 @@
 # Calcule a área, em cm², de um triângulo retângulo, recebendo a base e a altura. Exiba o resultado na tela
+
+print('*** CALCULANDO A ÁREA DE UM TRIÂNGULO RETÂNGULO ***')
+base = float(input('Digite a base: '))
+altura = float(input('Digite a altura: '))
+
+area = (base * altura) / 2
+
+print(f'\nA área do triângulo é {area:.1f}m²')
