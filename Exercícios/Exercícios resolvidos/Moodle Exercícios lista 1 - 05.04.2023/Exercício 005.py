407341c275e343c953689bfc443e7d2359d05a3a
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-18 16:10:39 -0300

    Update Exercício 005.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 005.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 005.py
@@ -1,1 +1,5 @@
 # Receba uma string do usuário e imprima a quantidade de caracteres dessa string, exibindo na tela o resultado
+
+texto = input('Digite algo: ')
+
+print(f'\nA quantidade de caracteres é {len(texto)}')
