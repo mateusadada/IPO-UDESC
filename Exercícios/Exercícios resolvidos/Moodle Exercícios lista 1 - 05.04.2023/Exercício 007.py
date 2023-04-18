499b1e7edad8d172ce4a1edc642b409113cedb67
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-18 16:25:39 -0300

    Update Exercício 007.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 007.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 007.py
@@ -1,1 +1,11 @@
 # Calcule a área de um círculo cujo raio será informado pelo usuário e exiba o resultado na tela
+
+import math
+
+raio = float(input('Digite o raio de um círculo (cm²): '))
+
+if raio < 0:
+    print('\nErro! O raio informado é negativo')
+else:
+    area = (raio ** 2) * math.pi
+    print(f'\nA área do círculo é {area:.2f}cm²')
