3f592e23aee7437326dd7172cd1726409c1752b1
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-19 13:52:17 -0300

    Update Exercício 008.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 008.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 008.py
@@ -5,3 +5,23 @@
 #  - cada litro pinta 4m²
 #  - a altura e o raio do tanque serão informados pelo usuário
 #  - mostre na tela a área do tanque, a quantidade de latas e o custo total
+
+from math import pi, ceil
+
+altura = float(input('Digite a altura do tanque: '))
+raio = float(input('Digite o raio do tanque: '))
+
+area = (raio + altura) * 2 * raio * pi
+litro = area / 4
+if litro != int(litro):
+    litro = ceil(litro)
+
+lata = litro / 5
+if lata != int(lata):
+    lata = ceil(lata)
+
+custo_total = lata * 120
+
+print(f'\nÁrea do tanque: {area:.2f}m²'
+      f'\nQuantidade de latas necessárias: {lata}'
+      f'\nCusto total: R$ {custo_total:.2f} reais')
