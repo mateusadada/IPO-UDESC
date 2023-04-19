a82d3e1ff763c7045543eb482802513fe65207d3
Mateus Adada <mateusadada@gmail.com>
2023-04-19 19:52:42 -0300

    Update Exercício 010.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 010.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 010.py
@@ -1,1 +1,16 @@
 # Digite uma frase e imprima a quantidade de vogais presentes na frase
+
+print('Bem vindo! Este programa informa a quantidade de vogais em um texto.')
+
+texto = input('Digite uma frase: ')
+
+letras = 'aeiouAEIOU'  # caracteres para procurar e somar
+quantidade_total = len(letras)
+i = 0
+soma_total = 0
+
+while i < quantidade_total:
+    soma_total += texto.count(letras[i])
+    i += 1
+
+print(f'\nA quantidade de vogais na frase é {soma_total}')
