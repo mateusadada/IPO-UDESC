8931a31026607c38d6d57c566897326869be465f
Mateus Adada <mateusadada@gmail.com>
2023-04-18 23:23:52 -0300

    Update Exercício 004.py
    Added a note about code in general

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 004.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 004.py
@@ -1,3 +1,5 @@
 # Faça um programa para receber as strings de um madlib. Utilize o seguinte texto como base:
 # hoje, eu fui no (lugar) com meu (substantivo). Fizemos muitas compras, já que em casa estava faltando (substantivo).
 # Depois disso, a gente foi na praia, onde vimos muitas pessoas (adjetivo) (verbo no gerúndio)
+
+print('Exercício não realizado por recomendação do professor devido não termos visto o conteúdo em aula')
