774269ced5963d6dd251bbfd7fa40bc74447eab6
Mateus Adada <mateusadada@gmail.com>
2023-04-18 23:41:24 -0300

    Update Exercício 009.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 009.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 009.py
@@ -4,10 +4,12 @@
 #  - o inverso do primeiro valor vezes o segundo multiplicado por 5
 #  - o quadrado do primeiro dividido pelo segundo elevado a -2
 
+print('Bem vindo! Este programa recebe dois números e realiza várias operações.')
+
 numero1 = int(input('Digite um número inteiro: '))
 numero2 = int(input('Digite outro número inteiro: '))
 
 print(f'\nO primeiro valor vezes o segundo: {numero1 * numero2}'
       f'\nO segundo valor menos o primeiro: {numero2 - numero1}'
-      f'\nO inverso do primeiro valor vezes o segundo multiplicado por 5: {((numero1 ** 1/2) * numero2) * 5:.1f}'
-      f'\nO quadrado do primeiro dividido pelo segundo elevado a -2: {((numero1 ** 2) / numero2) ** -2}')
+      f'\nO inverso do primeiro valor vezes o segundo multiplicado por 5: {((numero1 * -1) * (numero2 * 5)):.1f}'
+      f'\nO quadrado do primeiro dividido pelo segundo elevado a -2: {((numero1 ** 2) / (numero2 ** -2))}')
