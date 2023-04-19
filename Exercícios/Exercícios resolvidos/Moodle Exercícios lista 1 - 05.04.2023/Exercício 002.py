1210fedbcbd9b8b8f414702dd54de5f6ff129382
Mateus Adada <mateusadada@gmail.com>
2023-04-18 23:20:32 -0300

    Update Exercício 002.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 002.py
+++ Exercícios/Exercícios resolvidos/Moodle Exercícios lista 1 - 05.04.2023/Exercício 002.py
@@ -1,6 +1,8 @@
 # Receba a temperatura em Celsius de um usuário e converta para Fahrenheit, exibindo o resultado na tela
 
-celsius = float(input('Digite a temperatura em Celsius: '))
+print('Bem vindo! Este programa converte a temperatura em celsius para fahrenheit.')
+
+celsius = float(input('Digite a temperatura em celsius: '))
 fahrenheit = (1.8 * celsius) + 32
 
 print(f'\nA temperatura de {celsius:.1f}ºC é equivalente a {fahrenheit:.1f}ºF')
