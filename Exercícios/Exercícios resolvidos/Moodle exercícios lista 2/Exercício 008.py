970fe9278797fb3363cead20edf1fcc10f19f60d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-20 16:31:08 -0300

    Create Exercício 008.py
    Added code objective

---
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 008.py
@@ -0,0 +1,2 @@
+# Faça um programa que pergunte em que turno você estuda. Peça para digitar M-Matutino, V-Vespertino ou N-Noturno. 
+# - Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
