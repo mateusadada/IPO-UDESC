ff30377af5aa9cdda176ff322f97e9f719eec93e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-20 16:28:31 -0300

    Create Exercício 006.py
    Added code objective

---
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 006.py
@@ -0,0 +1,2 @@
+# Faça um programa que leia a idade e a altura de uma pessoa e verifique se ela pode entrar em um parque de diversões.
+# Para entrar no parque a pessoa deve ter pelo menos 12 anos e medir mais de 1,50m.
