fe514045dafe0077e113d15b9bd93e3fe260c551
Mateus Adada <mateusadada@gmail.com>
2023-04-21 14:57:16 -0300

    Update Exercício 004
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 004.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 004.py
@@ -1,1 +1,12 @@
 # Faça um programa que calcule se um ano é ou não bissexto (se necessário, procure a regra na internet)
+
+print('Bem vindo! Este programa informa se um ano é ou não bissexto.')
+
+ano = int(input('Digite um ano: '))
+
+if ano < 1:
+    print('\nErro! Não existe ano bissexto negativo')
+elif ano % 4 == 0:
+    print(f'\nO ano {ano} é bissexto')
+else:
+    print(f'\nO ano {ano} NÃO é bissexto')
