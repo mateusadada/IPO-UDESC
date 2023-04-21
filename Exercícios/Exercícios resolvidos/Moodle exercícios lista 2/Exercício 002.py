f28696282982249b6949316c6a480002cdbd4972
Mateus Adada <mateusadada@gmail.com>
2023-04-21 14:42:42 -0300

    Update Exercício 002
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 002.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 002.py
@@ -1,1 +1,12 @@
 # Escreva um programa que receberá um número e dirá se ele é positivo, negativo ou nulo
+
+print('Bem vindo! Este programa informa se um número é positivo, negativo ou nulo.')
+
+numero = int(input('Digite um número: '))
+
+if numero < 0:
+    print(f'\nO número {numero} é negativo')
+elif numero > 0:
+    print(f'\nO número {numero} é positivo')
+else:
+    print(f'\nO número {numero} é nulo')
