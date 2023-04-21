cb37a0e0b8690f4cde5b9f69193d2d2d856379a3
Mateus Adada <mateusadada@gmail.com>
2023-04-21 15:59:48 -0300

    Update Exercício 004.py
    Added condição do ano ser divisível por 100, mas não por 400

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 004.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 004.py
@@ -6,7 +6,9 @@ ano = int(input('Digite um ano: '))
 
 if ano < 1:
     print('\nErro! Não existe ano bissexto negativo')
-elif ano % 4 == 0:
-    print(f'\nO ano {ano} é bissexto')
-else:
+elif ano % 4 != 0:
     print(f'\nO ano {ano} NÃO é bissexto')
+elif ano % 100 == 0 and ano % 400 != 0:
+    print(f'\nO ano {ano} NÃO é bissexto')
+else:
+    print(f'\nO ano {ano} é bissexto')
