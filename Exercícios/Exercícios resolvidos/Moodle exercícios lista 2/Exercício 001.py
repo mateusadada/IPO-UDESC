6d70064eb24bcc6d3f2d66619ca4b7c27d41c210
Mateus Adada <mateusadada@gmail.com>
2023-04-20 21:42:45 -0300

    Update Exercício 001.py
    Added the code

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 001.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 001.py
@@ -1,1 +1,12 @@
 # Faça um programa que verifique se uma pessoa é ou não maior de idade
+
+print('Bem vindo! Este programa verifica se uma pessoa é ou não maior de idade')
+
+idade = int(input('Digite a idade: '))
+
+if idade < 0:
+    print('\nErro! Não existe idade negativa')
+elif idade < 18:
+    print('\nA pessoa NÃO é maior de idade')
+else:
+    print('\nA pessoa é maior de idade')
