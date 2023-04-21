20c1d7880d161f3a177d8fcd59a4602e91cfff9b
Mateus Adada <mateusadada@gmail.com>
2023-04-21 14:46:14 -0300

    Update Exercício 003.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 003.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 003.py
@@ -1,1 +1,10 @@
 # Calcule se um número é par ou ímpar
+
+print('Bem vindo! Este programa informa se um número é par ou ímpar.')
+
+numero = int(input('Digite um número: '))
+
+if numero % 2 == 0:
+    print(f'\nO número {numero} é par')
+else:
+    print(f'\nO número {numero} é ímpar')
