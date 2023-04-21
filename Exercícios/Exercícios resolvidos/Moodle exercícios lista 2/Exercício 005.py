ee41ccd6f2e289037ae5a9251fcc65d72e62e4d7
Mateus Adada <mateusadada@gmail.com>
2023-04-21 14:59:35 -0300

    Update Exercício 005.py
    Replaced code objective

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 005.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 005.py
@@ -1,1 +1,2 @@
-# Faça um programa que leia duas notas de um aluno e calcule a média. Se a média for maior ou igual a 6 está aprovado
+# Faça um programa que leia duas notas de um aluno e calcule a média. Se a média for maior ou igual a 6
+# está em recuperação, se for maior ou igual a 8 está aprovado, caso contrário, está reprovado
