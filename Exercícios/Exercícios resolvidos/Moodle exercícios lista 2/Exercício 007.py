ca8ba0c3085c8b834fa67b290311e5085cbdaad2
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-24 11:38:09 -0300

    Update Exercício 007.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 007.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 007.py
@@ -1,1 +1,15 @@
 # Faça um programa que peça dois números e imprima o maior deles.
+
+print('Bem vindo! Este programa informa qual é o maior número entre dois.')
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+maior = numero_1
+
+if numero_1 == numero_2:
+    print(f'\nOs dois números são iguais ({numero_1})')
+elif numero_2 > numero_1:
+    print(f'\nO segundo número ({numero_2}) é maior que o primeiro ({numero_1})')
+else:
+    print(f'\nO primeiro número ({numero_1}) é maior que o segundo ({numero_2})')
