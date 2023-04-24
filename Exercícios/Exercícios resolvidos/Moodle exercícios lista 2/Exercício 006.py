ca5694d94f55b14e2c8defebe9a0472e2c617128
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-24 09:56:48 -0300

    Update Exercício 006.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 006.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 006.py
@@ -1,2 +1,12 @@
 # Faça um programa que leia a idade e a altura de uma pessoa e verifique se ela pode entrar em um parque de diversões.
 # Para entrar no parque a pessoa deve ter pelo menos 12 anos e medir mais de 1,50m.
+
+print('Bem vindo! Este programa verifica se uma pessoa pode entrar no parque de diversões.')
+
+idade = int(input('Digite a idade: '))
+altura = float(input('Digite a altura (m): '))
+
+if idade >= 12 and altura > 1.5:
+    print('\nA pessoa pode entrar!')
+else:
+    print('\nInfelizmente, a pessoa não pode entrar.')
