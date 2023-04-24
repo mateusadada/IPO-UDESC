dd63073918bbb06c178e335ad4e9b4ecc9206d9e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-24 13:29:08 -0300

    Update Exercício 008.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 008.py
+++ Exercícios/Exercícios resolvidos/Moodle exercícios lista 2/Exercício 008.py
@@ -1,2 +1,17 @@
 # Faça um programa que pergunte em que turno você estuda. Peça para digitar M-Matutino, V-Vespertino ou N-Noturno. 
 # - Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+print('Bem vindo! Este programa faz uma saudação a depender do turno que você estuda.')
+
+turno = input('Em qual turno você estuda?'
+              '\nOpções: M-Matutino, V-Vespertino ou N-Noturno'
+              '\nResposta: ')
+
+if turno == 'M' or turno == 'm':
+    print('\nBom dia!')
+elif turno == 'V' or turno == 'v':
+    print('\nBoa tarde!')
+elif turno == 'N' or turno == 'n':
+    print('\nBoa noite!')
+else:
+    print('\nValor inválido!')
