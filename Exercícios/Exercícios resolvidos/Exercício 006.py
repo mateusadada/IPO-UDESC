be6f55fab88100fe5f0a9cab1c7a41aa248864c0
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-25 14:24:54 -0300

    Update Exercício 006.py
    Added a note about code function and replaced syntax

--- Exercícios/Exercícios resolvidos/Exercício 006.py
+++ Exercícios/Exercícios resolvidos/Exercício 006.py
@@ -1,8 +1,10 @@
 # Exemplo 2 do PDF Slides da aula - Parte 5
 
-idade = int(input('Digite a idade do seu carro: '))
+print('Bem vindo ao programa que informa a situação do carro!')
+
+idade = int(input('Digite a idade do carro: '))
 
 if idade <= 3:
-    print('\nSeu carro é seminovo')
+    print('\nO carro é seminovo')
 if idade > 3:
-    print('\nSeu carro é usado')
+    print('\nO carro é usado')
