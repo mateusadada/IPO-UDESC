de8db0ea7edb033a336313082e8eb3dee4d18a17
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-25 14:25:47 -0300

    Update Exercício 007.py
    Added a note about code function and replaced syntax

--- Exercícios/Exercícios resolvidos/Exercício 007.py
+++ Exercícios/Exercícios resolvidos/Exercício 007.py
@@ -1,9 +1,11 @@
 # Exemplo 3 do PDF Slides da aula - Parte 5
 # Mesmo exercício que o anterior (exemplo 2), mas utilizando o else
 
-idade = int(input('Digite a idade do seu carro: '))
+print('Bem vindo ao programa que informa a situação do carro!')
+
+idade = int(input('Digite a idade do carro: '))
 
 if idade <= 3:
-    print('\nSeu carro é seminovo')
+    print('\nO carro é seminovo')
 else:
-    print('\nSeu carro é usado')
+    print('\nO carro é usado')
