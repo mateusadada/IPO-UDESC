969a5d22923e856e110b15b8212dff7283de704d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-25 14:19:49 -0300

    Update Exercício 002.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 002.py
+++ Exercícios/Exercícios resolvidos/Exercício 002.py
@@ -1,6 +1,8 @@
 # Faça um algoritmo que leia as 3 notas de um aluno e calcule a média final deste aluno. Considerar que a
 # média é ponderada e que o peso das notas é: 2, 3 e 5, respectivamente.
 
+print('Bem vindo ao programa de cálculo da média final (ponderada) de um aluno!')
+
 nota1 = float(input('Digite a nota 1: '))
 nota2 = float(input('Digite a nota 2: '))
 nota3 = float(input('Digite a nota 3: '))
