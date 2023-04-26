5148491705aa26c402de22308559a5c80d9b667f
Mateus Adada <mateusadada@gmail.com>
2023-04-26 20:19:00 -0300

    Create Exercícios 032, 033 and 034
    Added the codes and a notes about codes functions

---
+++ Exercícios/Exercícios resolvidos/Exercício 032.py
@@ -0,0 +1,11 @@
+print('Bem vindo ao programa de cálculo da média de cinco notas pré-estabelecidas sem informar quais são!')
+
+notas = [6, 7, 5, 8, 9]
+soma = 0
+i = 0
+
+while i < 5:
+    soma += notas[i]
+    i += 1
+
+print(f'\nA média das notas é {soma / i:.2f}')
---
+++ Exercícios/Exercícios resolvidos/Exercício 033.py
@@ -0,0 +1,12 @@
+print('Bem vindo ao programa de cálculo da média de cinco notas informadas!')
+
+notas = [0, 0, 0, 0, 0]
+soma = 0
+i = 0
+
+while i < 5:
+    notas[i] = float(input(f'Digite a {i + 1}º nota: '))
+    soma += notas[i]
+    i += 1
+
+print(f'\nA média é {soma / i:.2f}')
