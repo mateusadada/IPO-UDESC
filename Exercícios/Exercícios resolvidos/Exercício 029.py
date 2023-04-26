1a81e0559039731c35eec2c56adae21b9c8e4c49
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 11:18:10 -0300

    Update Exercício 029.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 029.py
+++ Exercícios/Exercícios resolvidos/Exercício 029.py
@@ -1,6 +1,8 @@
 # Faça um programa que exiba uma lista de opções (menu): adição, subtração, divisão, multiplicação e sair.
 # Imprima a tabuada da operação escolhida. Repita até que a opção sair seja escolhida
 
+print('Bem vindo ao programa de cálculo de adição, subtração, divisão e multiplicação!')
+
 while True:
     i = 1
     print('\n*** MENU DE TABUADAS ***'
