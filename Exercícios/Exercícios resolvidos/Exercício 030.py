98ddccc34058f064ec71fb09905ff24beef2fe24
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 11:18:37 -0300

    Update Exercício 030.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 030.py
+++ Exercícios/Exercícios resolvidos/Exercício 030.py
@@ -3,6 +3,8 @@
 # números ímpares até o número lido. Se o resto de uma dessas divisões for igual a zero,
 # o número não é primo. Observe que 0 e 1 não são primos e que 2 é o único primo que é par
 
+print('Bem vindo ao programa de validação de número primo!')
+
 numero = int(input('Digite um número inteiro: '))
 
 i = 3
