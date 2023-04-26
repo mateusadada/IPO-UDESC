3ee7540532f0f8479f0c5100ad052802a5a60780
Mateus Adada <mateusadada@gmail.com>
2023-04-26 20:33:11 -0300

    Update Exercício 034.py
    Added the code and a note about code function

---
+++ Exercícios/Exercícios resolvidos/Exercício 034.py
@@ -0,0 +1,11 @@
+print('Bem vindo ao programa de cálculo da média de notas informadas!')
+
+quantidade = int(input('Digite a quantidade de notas: '))
+soma = 0
+notas = []
+
+for i in range(0, quantidade):
+    notas.append(float(input(f'Informe a {i + 1}º nota: ')))
+    soma += notas[i]
+
+print(f'\nA média das {quantidade} notas é {soma / quantidade:.2f}')
