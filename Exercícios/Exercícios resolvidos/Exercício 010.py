1d1b9b370c93c931151a41f284f708b41387bf1c
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 11:08:42 -0300

    Update Exercício 010.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 010.py
+++ Exercícios/Exercícios resolvidos/Exercício 010.py
@@ -1,5 +1,7 @@
 # Faça um programa que leia três números inteiros, em seguida mostre o maior e o menor deles.
 
+print('Bem vindo ao programa que mostra o maior valor entre três números!')
+
 numero1 = int(input('Digite o 1º valor: '))
 numero2 = int(input('Digite o 2º valor: '))
 numero3 = int(input('Digite o 3º valor: '))
