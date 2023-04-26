fd0b0ad11218d750c86f3e333de9f45a47c9bb7e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 11:11:18 -0300

    Update Exercício 017.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 017.py
+++ Exercícios/Exercícios resolvidos/Exercício 017.py
@@ -1,5 +1,7 @@
 # Faça um programa para imprimir os números ímpares de 1 até o número digitado pelo usuário
 
+print('Bem vindo ao programa que exibe os números ímpares de 1 até o limite informado!')
+
 fim = int(input('Digite um número positivo: '))
 x = 1
 
