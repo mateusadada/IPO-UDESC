31236eb49f4ddfb268682dcc8735c47f8cdbb518
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 11:07:58 -0300

    Update Exercício 008.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 008.py
+++ Exercícios/Exercícios resolvidos/Exercício 008.py
@@ -1,5 +1,7 @@
 # Exemplo 4 do PDF Slides da aula - Parte 5
 
+print('Bem vindo ao programa que mostra o preço do produto dependendo da categoria!')
+
 categoria = int(input('Digite a categoria do produto (1 a 5): '))
 
 if categoria == 1:
