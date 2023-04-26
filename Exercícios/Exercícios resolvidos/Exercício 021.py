c683b38e4c00b97261826103a9a0eeeeea9b679f
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-26 11:13:26 -0300

    Update Exercício 021.py
    Added a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 021.py
+++ Exercícios/Exercícios resolvidos/Exercício 021.py
@@ -1,5 +1,7 @@
 # Faça um programa de forma que o usuário também digite o início e o fim da tabuada, em vez de começar com 1 até 10
 
+print('Bem vindo ao programa que exibe a tabuada de um número informado!')
+
 inicio = int(input('Digite o início de uma tabuada: '))
 fim = int(input('Digite o fim de uma tabuada: '))
 
