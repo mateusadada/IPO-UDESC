389e2b038308089b0ca6d4dd0c02443d3348597b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-04-28 13:04:13 -0300

    Update Exercício 042.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 042.py
+++ Exercícios/Exercícios resolvidos/Exercício 042.py
@@ -1,1 +1,13 @@
 # Faça um programa para apagar fatias inteiras de uma só vez usando o range.
+
+from time import sleep
+
+lista = list(range(1, 101))
+
+print(f'\nLista: {lista}'
+      f'\n\nApagando o número 2 até o 99...')
+sleep(4)
+
+del lista[1:98]
+
+print(f'\nLista: {lista}')
