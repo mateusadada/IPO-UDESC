16c15f83e38f41b3e3f84668a086730d02ea19dc
Mateus Adada <mateusadada@gmail.com>
2023-04-27 21:55:08 -0300

    Update Exercício 041.py
    Added the code

--- Exercícios/Exercícios resolvidos/Exercício 041.py
+++ Exercícios/Exercícios resolvidos/Exercício 041.py
@@ -1,1 +1,25 @@
 # Faça um programa para retirar elementos de uma lista usando del.
+
+lista = list(range(1, 11))
+
+while True:
+    print(f'\nElementos ({len(lista)}): {lista}')
+    if len(lista) == 0:
+        print('Não há mais nenhum número na lista!')
+        break
+
+    numero = int(input('Deseja retirar qual número (0 p/ sair)? '))
+
+    if numero < 0:
+        print('Erro! Digite um número positivo ou nulo')
+    elif numero == 0:
+        print('Saindo... Até logo!')
+        break
+    else:
+        for i, e in enumerate(lista):
+            if e == numero:
+                print(i)
+                del lista[i]
+                break
+        else:
+            print('O número digitado não está na lista')
