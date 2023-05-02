3b173a54729787d7e7213fe0bb80cbe388070918
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-02 16:16:31 -0300

    Update Exercício 044.py
    Added the code and a note about code function

--- Exercícios/Exercícios resolvidos/Exercício 044.py
+++ Exercícios/Exercícios resolvidos/Exercício 044.py
@@ -1,1 +1,16 @@
 # Faça um programa para pesquisar se um elementos está em uma lista usando for.
+
+print('Bem vindo ao programa que pesquisa se um número está em uma lista!')
+
+lista = list(range(1, 200, 22))
+
+print(f'Lista: {lista}')
+
+numero = int(input('Digite um número para pesquisar: '))
+
+for indice, valor in enumerate(lista):
+    if valor == numero:
+        print(f'\nO número {numero} foi encontrado na posição {indice + 1}')
+        break
+else:
+    print(f'\nO número {numero} não está na lista')
