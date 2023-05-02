172e87f6b43fdc288a5058c47bd13e4ab9d00c86
Mateus Adada <mateusadada@gmail.com>
2023-05-02 00:53:00 -0300

    Update Exercício 043.py
    Add the code, a note about code function and replaced syntax/semantics

--- Exercícios/Exercícios resolvidos/Exercício 043.py
+++ Exercícios/Exercícios resolvidos/Exercício 043.py
@@ -1,3 +1,19 @@
-# Faça um programa para pesquisar se um elementos está em uma lista usando while.
+# Faça um programa para pesquisar se um elemento está em uma lista usando while.
 
-print('Bem vindo ao programa que verifica se tem um número na lista!')
+print('Bem vindo ao programa que pesquisa se um número está em uma lista!')
+
+lista = list(range(1, 150, 17))
+i = 0
+
+print(f'Lista: {lista}')
+
+numero = int(input('Digite um número para pesquisar: '))
+
+while i < len(lista):
+    if numero == lista[i]:
+        print(f'\nO número {numero} foi encontrado na posição {i + 1}.')
+        break
+
+    i += 1
+else:
+    print(f'\nNão foi encontrado o número {numero} na lista.')
