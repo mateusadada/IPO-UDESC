07d41eff489e288c26c6ed666cd5bb1f7e4bc4fd
Mateus Adada <mateusadada@gmail.com>
2023-05-03 19:24:57 -0300

    Update Exercício 049.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 049.py
+++ Exercícios resolvidos/Exercício 049.py
@@ -1,3 +1,7 @@
 # Inicializar um vetor de 10 inteiros com o valor zero e imprimir.
 
 print('Bem vindo ao programa que exibe dez zeros!')
+
+lista = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+print(f'\nLista: {lista}')
