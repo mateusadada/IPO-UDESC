0da26c4e77d77fd1edcff38f3a4c618c830df2ca
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-03 16:16:45 -0300

    Update Exercício 047.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 047.py
+++ Exercícios resolvidos/Exercício 047.py
@@ -1,1 +1,17 @@
 # Faça um programa para copiar os elementos pares da lista V para a P e os ímpares do V para a I.
+
+print('Bem vindo ao programa que separa os números pares dos ímpares!')
+
+V = list(range(1, 21))
+P = []
+I = []
+
+for e in V:
+    if e % 2 == 0:
+        P.append(e)
+    else:
+        I.append(e)
+
+print(f'Lista: {V}'
+      f'\n\nPares: {P}'
+      f'\nÍmpares: {I}')
