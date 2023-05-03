8fcf8ad5810622a0a14e2c0f873ecb0e9d218bd4
Mateus Adada <mateusadada@gmail.com>
2023-05-03 19:32:58 -0300

    Update Exercício 051.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 051.py
+++ Exercícios resolvidos/Exercício 051.py
@@ -1,1 +1,15 @@
 # Ler 5 inteiros do teclado e armazenar num vetor. Depois percorrer este vetor mostrando os números ímpares.
+
+print('Bem vindo ao programa que exibe os números ímpares digitados!')
+
+numeros = []
+impares = []
+
+for e in range(0, 5):
+    numeros.append(int(input(f'Digite um número inteiro ({e + 1}/5): ')))
+
+for e in numeros:
+    if e % 2 != 0:
+        impares.append(e)
+
+print(f'\nOs números ímpares são {impares}')
