ff38b42610efb598c1efcb91a8e7bac6852f8ca4
Mateus Adada <mateusadada@gmail.com>
2023-05-03 20:29:07 -0300

    Update Exercício 054.py
    Added the code and a note about code function. Replaced the syntax

--- Exercícios resolvidos/Exercício 054.py
+++ Exercícios resolvidos/Exercício 054.py
@@ -1,2 +1,17 @@
-# Dado os vetores A=[10, 11, 12, 13, 14, 15, 16,17] e B=[1, 2, 3, 4, 5, 6, 7, 8], fazer um programa para somar os dois
+# Dado os vetores A=[10, 11, 12, 13, 14, 15, 16, 17] e B=[1, 2, 3, 4, 5, 6, 7, 8], fazer um programa para somar os dois
 # vetores e armazenar o resultado em um vetor C ( C[i]=A[i]+B[i] ).
+
+print('Bem vindo ao programa de cálculo de soma de duas listas!')
+
+A = [10, 11, 12, 13, 14, 15, 16, 17]
+B = [1, 2, 3, 4, 5, 6, 7, 8]
+C = []
+
+print(f'\nLista A: {A}'
+      f'\nLista B: {B}'
+      f'\nLista C: {C}')
+
+for i, e in enumerate(A):
+    C.append(A[i] + B[i])
+
+print(f'\nLista C com a soma de A e B: {C}')
