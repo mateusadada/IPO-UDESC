2161945a8d00b7ef232d48de75326452fb624114
Mateus Adada <mateusadada@gmail.com>
2023-05-03 19:27:02 -0300

    Update Exercício 050.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 050.py
+++ Exercícios resolvidos/Exercício 050.py
@@ -1,1 +1,7 @@
 # Inicializar um vetor com os valores 1,2,3,4,5,6,7,8,9,10 e depois imprimir.
+
+print('Bem vindo ao programa que exibe os valores de 1 até 10!')
+
+lista = list(range(1, 11))
+
+print(f'\nLista: {lista}')
