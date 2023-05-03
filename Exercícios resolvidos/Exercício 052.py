1d09ee30fbaf95d8e08824b09e7aa3fbf115cb6f
Mateus Adada <mateusadada@gmail.com>
2023-05-03 19:44:29 -0300

    Update Exercício 052.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 052.py
+++ Exercícios resolvidos/Exercício 052.py
@@ -1,1 +1,13 @@
 # Inicializar um vetor com valores 2, 4, 6, 8, 10. Calcular e imprimir a soma dos valores.
+
+print('Bem vindo ao programa de cálculo da média dos valores de uma lista!')
+
+lista = [2, 4, 6, 8, 10]
+soma = 0
+
+print(f'Lista: {lista}')
+
+for e in lista:
+    soma += e
+
+print(f'\nSoma dos valores da lista: {soma}')
