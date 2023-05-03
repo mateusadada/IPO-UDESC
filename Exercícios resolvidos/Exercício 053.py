3920430b5c254a2b4c74bf224962f22367d02313
Mateus Adada <mateusadada@gmail.com>
2023-05-03 19:53:29 -0300

    Update Exercício 053.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 053.py
+++ Exercícios resolvidos/Exercício 053.py
@@ -1,1 +1,15 @@
 # Ler e armazenar num vetor 5 números float fornecidos pelo usuário e calcular a média dos valores.
+
+print('Bem vindo ao programa de cálculo da média de cinco números!')
+
+numeros = []
+soma = 0
+
+for e in range(0, 5):
+    numeros.append(float(input(f'Digite um número ({e + 1}/5): ')))
+    soma += numeros[e]
+
+media = soma / 5
+
+print(f'\nLista: {numeros}'
+      f'\nA média dos valores informados é {media:.2f}')
