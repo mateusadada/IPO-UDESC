411b38474993bd8ea4555dc5b8cfa4e57968a4d0
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-09 12:43:50 -0300

    Update Exercício 055.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 055.py
+++ Exercícios resolvidos/Exercício 055.py
@@ -1,1 +1,14 @@
 # Escrever um programa que leia 5 números float e imprima-os em ordem inversa.
+
+print('Bem vindo ao programa que exibe a ordem inversa de uma lista!')
+
+lista = []
+
+for e in range(0, 5):
+    lista.append(float(input(f'Digite o {e + 1}º número: ')))
+
+print(f'\nLista: {lista}'
+      '\n\nLista inversa: ', end='')
+
+for e in range(4, -1, -1):
+    print(lista[e], end=', ')
