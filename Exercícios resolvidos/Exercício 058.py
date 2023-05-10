b821c90be75f9dc7c591e5b5fdeae2c594feba30
Mateus Adada <mateusadada@gmail.com>
2023-05-09 22:53:28 -0300

    Update Exercício 058.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 058.py
+++ Exercícios resolvidos/Exercício 058.py
@@ -1,2 +1,20 @@
 # Faça um programa de forma a realizar a mesma tarefa do exemplo localizado no slide 21, mas sem utilizar a variável
 # achou. Dica: observe a condição de saída do while
+# Faça um programa de pesquisa de elementos
+
+print('Bem vindo ao programa que pesquisa um número em uma lista!')
+
+lista = list(range(1, 100, 17))
+
+print(f'{lista}')
+numero = int(input('Digite um número para pesquisar na lista: '))
+
+x = 0
+
+while x < len(lista):
+    if lista[x] == numero:
+        print(f'\nFoi encontrado o número {numero} na posição {x + 1}')
+        break
+    x += 1
+else:
+    print(f'\nNão foi encontrado o número {numero} na lista')
