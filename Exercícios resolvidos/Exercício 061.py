94de8b08a4012044f1d1c864a906d6305e4a1b05
Mateus Adada <mateusadada@gmail.com>
2023-05-09 23:22:34 -0300

    Update Exercício 061.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 061.py
+++ Exercícios resolvidos/Exercício 061.py
@@ -1,1 +1,14 @@
 # Faça um programa para imprimir o menor elemento de uma lista.
+
+print('Bem vindo ao programa que exibe o menor número de uma lista!')
+
+lista = [5, 9, 7, 10, 1, 65, -74, 65, 87]
+menor = lista[0]
+
+print(f'Lista: {lista}')
+
+for e in lista:
+    if e < menor:
+        menor = e
+
+print(f'\nO menor número é {menor}')
