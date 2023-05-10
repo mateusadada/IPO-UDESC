7615d66e6f64c6cb4ec5168e264405732b6c72c7
Mateus Adada <mateusadada@gmail.com>
2023-05-09 23:26:16 -0300

    Update Exercício 061.py
    Removed a print

--- Exercícios resolvidos/Exercício 061.py
+++ Exercícios resolvidos/Exercício 061.py
@@ -5,10 +5,9 @@ print('Bem vindo ao programa que exibe o menor número de uma lista!')
 lista = [5, 9, 7, 10, 1, 65, -74, 65, 87]
 menor = lista[0]
 
-print(f'Lista: {lista}')
-
 for e in lista:
     if e < menor:
         menor = e
 
-print(f'\nO menor número é {menor}')
+print(f'Lista: {lista}'
+      f'\n\nO menor número é {menor}')
