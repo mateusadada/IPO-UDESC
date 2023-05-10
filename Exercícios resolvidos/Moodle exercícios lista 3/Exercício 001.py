805c10b6d574efdc021dc42ea0bdf76004d31a38
Mateus Adada <mateusadada@gmail.com>
2023-05-10 20:08:48 -0300

    Update Exercício 001.py
    Added the code and a note about code function

--- Exercícios resolvidos/Moodle exercícios lista 3/Exercício 001.py
+++ Exercícios resolvidos/Moodle exercícios lista 3/Exercício 001.py
@@ -1,3 +1,18 @@
 # Crie uma lista de números inteiros e exiba o valor máximo e mínimo.
 
 print('Bem vindo ao programa que exibe os valores máximo e mínimo de uma lista!')
+
+lista = [5, 4, 9, 8, 45, 36, -74, -85, 12, -74]
+maximo = lista[0]
+minimo = lista[0]
+
+print(f'Lista: {lista}')
+
+for e in lista:
+    if e > maximo:
+        maximo = e
+    if e < minimo:
+        minimo = e
+
+print(f'\nMáximo: {maximo}'
+      f'\nMínimo: {minimo}')
