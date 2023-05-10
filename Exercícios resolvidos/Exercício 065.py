4c8d88a20c2f480879fee790c500a14262f436ca
Mateus Adada <mateusadada@gmail.com>
2023-05-10 19:49:58 -0300

    Update Exercício 065.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 065.py
+++ Exercícios resolvidos/Exercício 065.py
@@ -1,1 +1,34 @@
 # Faça um programa que calcule a média de cinco notas informadas e mostre qual nota está mais próxima dela.
+
+print('Bem vindo ao programa que exibe a nota mais próxima da média de cinco notas!')
+
+notas = []
+diferenca_com_media = []
+soma = 0
+
+for i in range(5):
+    notas.append(float(input(f'{i + 1}º nota: ')))
+    soma += notas[i]
+
+media = soma / len(notas)
+print(f'\nNotas informadas: {notas}'
+      f'\nMédia: {media:.1f}')
+
+for i in range(len(notas)):
+    diferenca = notas[i] - media
+    if diferenca < 0:
+        diferenca = diferenca * - 1
+    diferenca_com_media.append(diferenca)
+
+for i in range(0, len(diferenca_com_media) - 1):
+    for j in range(1, len(diferenca_com_media)):
+        if diferenca_com_media[i] > diferenca_com_media[j]:
+            aux = diferenca_com_media[i]
+            diferenca_com_media[i] = diferenca_com_media[j]
+            diferenca_com_media[j] = aux
+
+            aux = notas[i]
+            notas[i] = notas[j]
+            notas[j] = aux
+
+print(f'\nNota mais próxima da média: {notas[0]}')
