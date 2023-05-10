4c2d500b3fd11e6c3cebf6448764de8935672b9f
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-10 13:05:14 -0300

    Update Exercício 063.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 063.py
+++ Exercícios resolvidos/Exercício 063.py
@@ -1,1 +1,16 @@
 # Faça um programa para ordenar a lista em ordem decrescente. L=[2,1,5,4,3] deve ser ordenada como L=[5,4,3,2,1].
+
+print('Bem vindo ao programa que exibe a lista em ordem decrescente!')
+
+lista = [2, 1, 5, 4, 3]
+
+print(f'Lista: {lista}')
+
+for i in range(0, len(lista) - 1):
+    for j in range(i + 1, len(lista)):
+        if lista[i] < lista[j]:
+            aux = lista[i]
+            lista[i] = lista[j]
+            lista[j] = aux
+
+print(f'\nLista ordenada: {lista}')
