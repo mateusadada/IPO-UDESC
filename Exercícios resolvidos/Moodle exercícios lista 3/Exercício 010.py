31782766374408abe49c18221711689563769ed8
Mateus Adada <mateusadada@gmail.com>
2023-05-10 23:56:15 -0300

    Update Exercício 010.py
    Added the code and a note about code function

--- Exercícios resolvidos/Moodle exercícios lista 3/Exercício 010.py
+++ Exercícios resolvidos/Moodle exercícios lista 3/Exercício 010.py
@@ -1,1 +1,19 @@
 # Crie uma lista de números e calcule a diferença entre o maior e o menor valor.
+
+print('Bem vindo ao programa de cálculo da diferença entre o maior e o menor valor!')
+
+lista = [4, 8, -54, 254, 21, -98, 54, 2, 74, 65, -42]
+maior = lista[0]
+menor = lista[0]
+
+for e in lista:
+    if e > maior:
+        maior = e
+    if e < menor:
+        menor = e
+
+diferenca = maior - menor
+
+print(f'\nMaior: {maior}'
+      f'\nMenor: {menor}'
+      f'\nDiferença entre os dois: {diferenca}')
