2fdbc45c281c4a8987023a1af3af48a7ad6ab632
Mateus Adada <mateusadada@gmail.com>
2023-05-11 00:02:04 -0300

    Update Exercício 005.py
    Added the code and a note about code function

--- Exercícios resolvidos/Moodle exercícios lista 3/Exercício 005.py
+++ Exercícios resolvidos/Moodle exercícios lista 3/Exercício 005.py
@@ -1,1 +1,13 @@
 # Crie uma lista de números e crie uma nova lista com os números pares.
+
+print('Bem vindo ao programa que cria uma nova lista com números pares de outra lista!')
+
+lista = list(range(1, 150, 7))
+lista_pares = []
+
+for e in lista:
+    if e % 2 == 0:
+        lista_pares.append(e)
+
+print(f'\nLista: {lista}'
+      f'\nLista dos pares: {lista_pares}')
