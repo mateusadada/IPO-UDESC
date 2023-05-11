a57209719738512609cd3d93192ce46871f4d7bb
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-11 13:18:09 -0300

    Update Exercício 048.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 048.py
+++ Exercícios resolvidos/Exercício 048.py
@@ -1,1 +1,11 @@
 # Faça um programa que ordene valores em uma lista.
+
+print('Bem vindo ao programa que ordena valores em uma lista!')
+
+lista = [5, 9, -7, 45, 0, 789, 41, -45, 98, 4, -41, 4]
+
+print(f'lista: {lista}')
+
+lista.sort()
+
+print(f'\nLista ordenada: {lista}')
