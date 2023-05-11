f53aadffb4be16d2f00a4305b8c6a6e66bf4c24c
Mateus Adada <mateusadada@gmail.com>
2023-05-11 00:09:14 -0300

    Update Exercício 004.py
    Added the code and a note about code function

--- Exercícios resolvidos/Moodle exercícios lista 3/Exercício 004.py
+++ Exercícios resolvidos/Moodle exercícios lista 3/Exercício 004.py
@@ -1,1 +1,13 @@
 # Crie uma lista de palavras e crie uma nova lista com as palavras que têm mais de 5 caracteres.
+
+print('Bem vindo ao programa que cria uma lista de palavras com mais de 5 caracteres de outra lista!')
+
+lista = ['casa', 'bola', 'tesoura', 'mansão', 'tecnologia', 'universidade', 'arte', 'design', 'moldura']
+nova_lista = []
+
+for e in lista:
+    if len(e) > 5:
+        nova_lista.append(e)
+
+print(f'\nLista: {lista}'
+      f'\nNova lista: {nova_lista}')
