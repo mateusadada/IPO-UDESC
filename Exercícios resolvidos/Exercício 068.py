7bd22fe3cce976d3bfdceb525e70e884ace79153
Mateus Adada <mateusadada@gmail.com>
2023-05-14 23:09:29 -0300

    Update Exercício 068.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 068.py
+++ Exercícios resolvidos/Exercício 068.py
@@ -1,1 +1,10 @@
 # Exemplo de retorno que verifica se valor é par ou não
+
+def e_par(x):
+    return x % 2 == 0
+
+
+print('Bem vindo ao programa que exibe se um número é par ou não!')
+print(f'2: {e_par(2)}'
+      f'\n3: {e_par(3)}'
+      f'\n10: {e_par(10)}')
