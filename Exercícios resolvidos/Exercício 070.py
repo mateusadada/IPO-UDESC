1e6c36d33f24cf519279c1ceadd0cb3219e4f2b9
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-16 12:47:30 -0300

    Update Exercício 070.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 070.py
+++ Exercícios resolvidos/Exercício 070.py
@@ -2,3 +2,19 @@
 # maior(5,6) = 6
 # maior(2,1) = 2
 # maior(7,7) = 7
+
+def maior_2_numeros(a, b):
+    if a > b:
+        return f'O maior número é {a}'
+    elif a < b:
+        return f'O maior número é {b}'
+    else:
+        return f'Os dois números ({a}) são iguais'
+
+
+print('Bem vindo ao programa que exibe qual número é maior entre dois!')
+
+numero_1 = int(input('1º número: '))
+numero_2 = int(input('2º número: '))
+
+print(f'\n{maior_2_numeros(numero_1, numero_2)}')
