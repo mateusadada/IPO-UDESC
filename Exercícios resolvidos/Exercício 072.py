342e230731f4147a8d857a4cefe96da28c15f8d3
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-16 16:37:37 -0300

    Update Exercício 072.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 072.py
+++ Exercícios resolvidos/Exercício 072.py
@@ -1,2 +1,11 @@
 # Escreva uma função que receba o lado de um quadrado e retorne sua área (A = lado²).
 # Exemplo de valores esperados: área_quadrado(4) = 16
+
+def area_quadrado(lado):
+    return pow(lado, 2)
+
+
+print('Bem vindo ao programa de cálculo da área de um quadrado!')
+lado_quadrado = float(input('Digite o lado m²: '))
+
+print(f'\nA área é {area_quadrado(lado_quadrado):.1f}m²')
