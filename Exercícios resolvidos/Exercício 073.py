71c433a376a69c85f849bde1e01315df90fb26bb
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-05-16 16:45:42 -0300

    Update Exercício 073.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 073.py
+++ Exercícios resolvidos/Exercício 073.py
@@ -1,2 +1,12 @@
 # Escreva uma função que receba a base e a altura de um triângulo e retorne sua área (A = (base x altura)/2).
 # Exemplo de valores esperados: área_triângulo(5,8) = 20
+
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+
+print('Bem vindo ao programa de cálculo da área de triângulo!')
+base = float(input('Digite a base (m²): '))
+altura = float(input('Digite a altura (m²): '))
+
+print(f'\nA área é {area_triangulo(base, altura):.1f}m²')
