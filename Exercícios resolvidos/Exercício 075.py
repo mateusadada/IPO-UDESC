dc108e896b0d13b8a042a9c5c1857ccdbfaf37c8
Mateus Adada <mateusadada@gmail.com>
2023-05-16 23:27:49 -0300

    Update Exercício 075.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 075.py
+++ Exercícios resolvidos/Exercício 075.py
@@ -1,1 +1,14 @@
 # Exemplo de cálculo da média
+
+def media(numeros):
+    total = 0
+    for e in numeros:
+        total += e
+    return total / len(numeros)
+
+
+lista = list(range(1, 50, 4))
+
+print('Bem vindo ao programa de cálculo da média de uma lista a partir de uma função!'
+      f'\nLista: {lista}'
+      f'\n\nMédia dos números: {media(lista):.1f}')
