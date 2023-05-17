418623c24a8d5848c29388c99a9a83a84203cf72
Mateus Adada <mateusadada@gmail.com>
2023-05-16 23:46:51 -0300

    Update Exercício 077.2.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 077.2.py
+++ Exercícios resolvidos/Exercício 077.2.py
@@ -1,1 +1,12 @@
 # Variáveis locais e globais (página 13)
+
+def muda_e_imprime():
+    a = 7
+    print('A dentro da função:', a)
+
+
+a = 5
+
+print('A antes de mudar:', a)
+muda_e_imprime()
+print('A depois de mudar:', a)
