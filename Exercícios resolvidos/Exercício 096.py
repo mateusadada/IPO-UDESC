203c24229674c99381f024cc844add77de51e0b0
Mateus Adada <mateusadada@gmail.com>
2023-06-05 23:33:11 -0300

    Update Exercício 096.py
    Added the code and a note about code function

--- Exercícios resolvidos/Exercício 096.py
+++ Exercícios resolvidos/Exercício 096.py
@@ -1,1 +1,19 @@
 # Método Construtor de um aparelho de tv. Pode-se fazer com a TV, por exemplo, mudança de canal, ligá-la ou desligá-la.
+
+class TV:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 2
+
+
+print('Bem vindo ao programa usando o método construtor!')
+
+tv_do_quarto = TV()
+print(tv_do_quarto.ligada)
+print(tv_do_quarto.canal)
+
+tv_da_sala = TV()
+tv_da_sala.ligada = True
+tv_da_sala.canal = 4
+print(tv_da_sala.ligada)
+print(tv_da_sala.canal)
