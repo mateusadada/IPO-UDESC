592d60d6367cf0c2075ae8d08d1f080672ca2789
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-06 12:35:29 -0300

    Update Exercício 100.py
    Added the code, a note about function and replaced syntax/semantics

--- Exercícios resolvidos/Exercício 100.py
+++ Exercícios resolvidos/Exercício 100.py
@@ -1,2 +1,33 @@
-# Atualmente, a classe TV inicializa o canal com 2. Modifique a classe TV de forma a receber o canal inicial
-# em seu construtor.
+# A classe TV inicializa o canal com 2. Modifique a classe TV para receber o canal inicial no seu construtor.
+
+class TV:
+    def __init__(self, canal, minimo, maximo):
+        self.ligada = False
+        self.canal_inical = canal
+        self.canal_minimo = minimo
+        self.canal_maximo = maximo
+
+    def muda_canal_para_baixo(self):
+        if self.canal_inical - 1 >= self.canal_minimo:
+            self.canal_inical -= 1
+
+    def muda_canal_para_cima(self):
+        if self.canal_inical + 1 <= self.canal_maximo:
+            self.canal_inical += 1
+
+
+print('Bem-vindo ao programa que exibe o canal máximo e mínimo de uma TV após mudar 120x!')
+
+TV_da_sala = TV(47, 1, 120)
+
+print(f'Canal inicial: {TV_da_sala.canal_inical}\n')
+
+for x in range(0, 120):
+    TV_da_sala.muda_canal_para_cima()
+
+print(f'Canal máximo após subir 120x: {TV_da_sala.canal_inical}')
+
+for x in range(0, 120):
+    TV_da_sala.muda_canal_para_baixo()
+
+print(f'Canal mínimo após descer 120x: {TV_da_sala.canal_inical}')
