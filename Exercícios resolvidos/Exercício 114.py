046da077c975b1d95eff7beacd4036c314f66c6b
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-06-13 13:08:01 -0300

    Update Exercício 114.py
    Added the code and a note about function

--- Exercícios resolvidos/Exercício 114.py
+++ Exercícios resolvidos/Exercício 114.py
@@ -3,3 +3,41 @@
 #   Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro
 # Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidas de um local. Depois, deve
 # criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés necessárias para o local.
+
+class Retangulo:
+    def __init__(self, base=0, altura=0):
+        self.base = base
+        self.altura = altura
+
+    def set_base(self, base):
+        self.base = base
+
+    def set_altura(self, altura):
+        self.altura = altura
+
+    def get_base(self):
+        return self.base
+
+    def get_altura(self):
+        return self.altura
+
+    def calcular_area(self, base, altura):
+        return self.base * self.altura
+
+    def calcular_perimetro(self, base, altura):
+        return (self.base + self.altura) * 2
+
+
+print('Bem vindo ao programa de cálculo de quantidade de pisos e rodapés para um local!')
+
+comprimento = float(input('Informe o comprimento (m²): '))
+largura = float(input('Largura (m²): '))
+
+valores = Retangulo()
+valores.set_base(comprimento)
+valores.set_altura(largura)
+
+print(f'\nComprimento informado: {valores.get_base()}m²'
+      f'\nLargura: {valores.get_altura()}m²'
+      f'\n\nQuantidade de pisos necessários: {valores.calcular_area(comprimento, largura):.2f}m²'
+      f'\nRodapés: {valores.calcular_perimetro(comprimento, largura)}m')
